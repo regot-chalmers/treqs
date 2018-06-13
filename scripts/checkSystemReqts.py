b36c48afdf0bd90cb6f270ad7c55d0ce41c506fb
grischa <grischa@chalmers.se>
2018-06-13 16:54:33 +0200

    Simplified system reqs script and commented it.
    Change-Id: Ic10522aa87de8d05c651891ffe665b02c56b9fe0

--- scripts/checkSystemReqts.py
+++ scripts/checkSystemReqts.py
@@ -1,9 +1,54 @@
 #!/usr/bin/python
 import getopt, os, fnmatch, re, sys
 
+#function defs
+#NOTE: Not self-contained/effect-free. It uses and mutates the sets in which ids are stored (storySet,noUSTracingSet,reqIDSet,duplicateIDSet)
+def processRequirementsLine( line ):
+	"This process a single line in a requirements file, extracting duplicate IDs, missing US traces, and a list of all traces to US."
+        
+	#Extracts the actual requirement tag if there is one. Note that this requires the tag to be in a single line.
+	m = re.search('<requirement .*?>', line)
+        if m:
+        	#print 'New requirement:'
+                reqtag = m.group(0)
+                #print reqtag
+                
+		#Extract the id attribute from within the requirement tag. Only requirements with id are processed.
+		id = re.search('(?<=id=).*?(?=[ >])', reqtag)
+                if id:
+                	id = id.group(0)
+                        #print id
+
+			#Find duplicate ids. Note that currently, duplicate ids are still processed further.
+                        if id in reqIDSet:
+                        	print 'Duplicate requirement id:',id
+                                duplicateIDSet.add(id)
+                        else:
+                        	reqIDSet.add(id)
+
+			#Find all issue attributes. Supports also multiple issue attributes in theory.
+                        stories = re.findall('(?<=issue=).*?(?=[ >])', reqtag)
+
+			#Find requirements without traces
+                        if len(stories)==0:
+                        	print 'Warning: Requirement is not traced to a user story!'
+                                noUSTracingSet.add(id)
+                        else:
+				for currentUS in stories:
+					#Support potential commas in an issue attribute
+                                	splitstories = re.split(',', currentUS)
+                                        for currentStory in splitstories:
+                                        	print currentStory
+                                                storySet.add(currentStory)
+		print ''
+	return
+
+#MAIN
+#default values for directory and recursive traversion.
 dir='./'
 recursive=False
 
+#argument options for this script. Only accepts the option d (root dir) and r (recursive)
 try:
 	opts, args = getopt.getopt(sys.argv[1:],"hd:r",["dir="])
 except getopt.GetoptError:
@@ -14,14 +59,16 @@ for opt, arg in opts:
         	dir = os.path.normpath(arg)
 	elif opt in ("-r"):
         	recursive = True
-print 'Directory is', dir
-print 'Recursive is', recursive 
+#print 'Directory is', dir
+#print 'Recursive is', recursive 
 
+#Sets for all ids
 storySet = set()
 noUSTracingSet = set()
 reqIDSet = set()
 duplicateIDSet = set()
 
+#recursive traversion of root directory
 if recursive:
 	for root, directories, filenames in os.walk(dir):
 	#	for directory in directories:
@@ -29,78 +76,38 @@ if recursive:
 
 		for filename in filenames: 
 			entry=os.path.join(root,filename) 
+			
+			#Only files ending on sys-reqts.md are scanned
 			pattern = "*sys-reqts.md"  
     			if fnmatch.fnmatch(entry, pattern):
-				file = open(entry, "r")
-				for line in file: 
-					#if line.find('<requirement',0,len(line))!=-1:
-					#	print line      
-					m = re.search('<requirement .*?>', line)
-					if m:
-						print 'New requirement:'
-						reqtag = m.group(0)
-						print reqtag
-						id = re.search('(?<=id=).*?(?=[ >])', reqtag)
-						if id:
-							id = id.group(0)
-							print id
-							if id in reqIDSet:
-								print 'Duplicate requirement id:',id
-								duplicateIDSet.add(id)
-							else:
-								reqIDSet.add(id) 
-							stories = re.findall('(?<=issue=).*?(?=[ >])', reqtag)
-							if len(stories)==0:
-								print 'Warning: Requirement is not traced to a user story!'
-								noUSTracingSet.add(id)
-							for currentUS in stories:
-								splitstories = re.split(',', currentUS)
-								for currentStory in splitstories:
-									print currentStory
-									storySet.add(currentStory)
-							print ''
+				with open(entry, "r") as file:
+					for line in file: 
+						processRequirementsLine(line)
 else:
 	listOfFiles = os.listdir(dir)
+	
+	#Only files ending on sys-reqts.md are scanned
 	pattern = "*sys-reqts.md"  
 	for entry in listOfFiles:	
 		if fnmatch.fnmatch(entry, pattern):
-                	file = open(os.path.join(dir,entry), "r")
-                        for line in file:
-	                        #if line.find('<requirement',0,len(line))!=-1:
-                                #       print line
-                                m = re.search('<requirement .*?>', line)
-                                if m:
-        	                        print 'New requirement:'
-                                        reqtag = m.group(0)
-                                        print reqtag
-                                        id = re.search('(?<=id=).*?(?=[ >])', reqtag)
-                                        if id:
-						id = id.group(0)
-                                                print id
-                                                if id in reqIDSet:
-                                                	print 'Duplicate requirement id:',id
-                                                        duplicateIDSet.add(id)
-                                                else:
-                                                        reqIDSet.add(id)	                                        
-						stories = re.findall('(?<=issue=).*?(?=[ >])', reqtag)
-        	                                if len(stories)==0:
-                	        	                print 'Warning: Requirement is not traced to a user story!'
-							noUSTracingSet.add(id)
-                        	                for currentUS in stories:
-                                		        splitstories = re.split(',', currentUS)
-                                        	        for currentStory in splitstories:
-                                        		        print currentStory
-                                                        	storySet.add(currentStory)
-						print ''
+                	with open(os.path.join(dir,entry), "r") as file:
+	                        for line in file:
+					processRequirementsLine(line)
 
+
+#Simple printouts of all relevant sets.
 print 'All stories:'
 for currentStory in storySet:
 	print currentStory
 print''
+
+
 print 'Requirements without traces to stories:'
 for currentID in noUSTracingSet:
 	print currentID
 print''
+
+
 print 'Duplicate requirements IDs:'
 for currentID in duplicateIDSet:
 	print currentID
