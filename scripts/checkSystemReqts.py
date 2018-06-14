1be61dfcbd8e0181c529dd1c5541b6447f11a0a4
rebekkaa <wohlrab@chalmers.se>
2018-06-14 11:30:16 +0200

    fixing issues with indentation, prints without parentheses

--- scripts/checkSystemReqts.py
+++ scripts/checkSystemReqts.py
@@ -8,39 +8,39 @@ def processRequirementsLine( line ):
         
 	#Extracts the actual requirement tag if there is one. Note that this requires the tag to be in a single line.
 	m = re.search('<requirement .*?>', line)
-        if m:
-        	#print 'New requirement:'
-                reqtag = m.group(0)
-                #print reqtag
-                
+	if m:
+		#print 'New requirement:'
+		reqtag = m.group(0)
+		#print reqtag
+
 		#Extract the id attribute from within the requirement tag. Only requirements with id are processed.
 		id = re.search('(?<=id=).*?(?=[ >])', reqtag)
-                if id:
-                	id = id.group(0)
-                        #print id
+		if id:
+			id = id.group(0)
+			#print id
 
 			#Find duplicate ids. Note that currently, duplicate ids are still processed further.
-                        if id in reqIDSet:
-                        	print 'Duplicate requirement id:',id
-                                duplicateIDSet.add(id)
-                        else:
-                        	reqIDSet.add(id)
+		if id in reqIDSet:
+			print ('Duplicate requirement id:',id)
+			duplicateIDSet.add(id)
+		else:
+			reqIDSet.add(id)
 
-			#Find all issue attributes. Supports also multiple issue attributes in theory.
-                        stories = re.findall('(?<=issue=).*?(?=[ >])', reqtag)
+		#Find all issue attributes. Supports also multiple issue attributes in theory.
+		stories = re.findall('(?<=issue=).*?(?=[ >])', reqtag)
 
-			#Find requirements without traces
-                        if len(stories)==0:
-                        	print 'Warning: Requirement is not traced to a user story!'
-                                noUSTracingSet.add(id)
-                        else:
-				for currentUS in stories:
-					#Support potential commas in an issue attribute
-                                	splitstories = re.split(',', currentUS)
-                                        for currentStory in splitstories:
-                                        	print currentStory
-                                                storySet.add(currentStory)
-		print ''
+		#Find requirements without traces
+		if len(stories)==0:
+			print('Warning: Requirement is not traced to a user story!')
+			noUSTracingSet.add(id)
+		else:
+			for currentUS in stories:
+				#Support potential commas in an issue attribute
+				splitstories = re.split(',', currentUS)
+				for currentStory in splitstories:
+					print(currentStory)
+					storySet.add(currentStory)
+		print('')
 	return
 
 #MAIN
@@ -52,7 +52,7 @@ recursive=False
 try:
 	opts, args = getopt.getopt(sys.argv[1:],"hd:r",["dir="])
 except getopt.GetoptError:
-      print 'Usage: '+sys.argv[0]+' -d <directory> -r'
+      print('Usage: '+sys.argv[0]+' -d <directory> -r')
       sys.exit(2)
 for opt, arg in opts:
 	if opt in ("-d", "--dir"):
@@ -78,8 +78,8 @@ if recursive:
 			entry=os.path.join(root,filename) 
 			
 			#Only files ending on sys-reqts.md are scanned
-			pattern = "*sys-reqts.md"  
-    			if fnmatch.fnmatch(entry, pattern):
+			pattern = "*sys-reqts.md"
+			if fnmatch.fnmatch(entry, pattern):
 				with open(entry, "r") as file:
 					for line in file: 
 						processRequirementsLine(line)
@@ -90,24 +90,24 @@ else:
 	pattern = "*sys-reqts.md"  
 	for entry in listOfFiles:	
 		if fnmatch.fnmatch(entry, pattern):
-                	with open(os.path.join(dir,entry), "r") as file:
-	                        for line in file:
+			with open(os.path.join(dir,entry), "r") as file:
+				for line in file:
 					processRequirementsLine(line)
 
 
 #Simple printouts of all relevant sets.
-print 'All stories:'
+print('All stories:')
 for currentStory in storySet:
-	print currentStory
-print''
+	print(currentStory)
+print('')
 
 
-print 'Requirements without traces to stories:'
+print('Requirements without traces to stories:')
 for currentID in noUSTracingSet:
-	print currentID
-print''
+	print(currentID)
+print('')
 
 
-print 'Duplicate requirements IDs:'
+print('Duplicate requirements IDs:')
 for currentID in duplicateIDSet:
-	print currentID
+	print(currentID)
